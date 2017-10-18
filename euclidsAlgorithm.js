58ca81581e52b00185592e6dd503fec93f2bc01d
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-10-17 22:56:44 -0400

    Added comments

--- euclidsAlgorithm.js
+++ euclidsAlgorithm.js
@@ -8,6 +8,7 @@ var remainder;
 //Creates our euclidsAlorithm function
 function euclidsAlgorithm(A, B){
 	//while the remainder of our two values is not 0
+	//value of B decreased by at least a factor of 1/2 for two trips through the loop (O(log B))
 	while(B != 0){
 		//if A is smaller then B, the values are swapped
 		remainder = A%B;
@@ -17,7 +18,7 @@ function euclidsAlgorithm(A, B){
 	}
 	//If the GCD of two integers is 1, A and B are said to be relatively prime or coprime 
 	if(A === 1) A = "coprime";
-	
+
 	console.log(A);
 	return A;
 }
